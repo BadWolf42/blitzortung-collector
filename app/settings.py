b2d9f72e9cbd5afd5f879e5af7d74da963c8ccad
Bad <BadWolf42@users.noreply.github.com>
2023-09-26 14:24:22 +0200

    Remove version from settings

--- app/settings.py
+++ app/settings.py
@@ -7,6 +7,5 @@ class Settings(BaseSettings):
     postgres_host: str  = 'db'
     postgres_port: str  = '5432'
     postgres_database: str  = 'blitzdb'
-    version: str = '2.1.0'
 
 settings = Settings()
